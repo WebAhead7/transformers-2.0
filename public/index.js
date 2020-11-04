7c7786a9b0907010d0bd0cca0a1e8978a589e375
Lujain AbdUllatif <lujain.abdallatif@mail.huji.ac.il>
2020-11-04 12:55:29 +0200

    count feature added & heruko script added

--- public/index.js
+++ public/index.js
@@ -1,62 +1,7 @@
 const autoCompleteApi = "getcar/";
 let grappedData;
-const testArr = [
-  {
-    Name: "chevrolet chevelle malibu",
-    Miles_per_Gallon: 18,
-    Cylinders: 8,
-    Displacement: 307,
-    Horsepower: 130,
-    Weight_in_lbs: 3504,
-    Acceleration: 12,
-    Year: "1970-01-01",
-    Origin: "USA",
-  },
-  {
-    Name: "buick skylark 320",
-    Miles_per_Gallon: 15,
-    Cylinders: 8,
-    Displacement: 350,
-    Horsepower: 165,
-    Weight_in_lbs: 3693,
-    Acceleration: 11.5,
-    Year: "1970-01-01",
-    Origin: "USA",
-  },
-  {
-    Name: "plymouth satellite",
-    Miles_per_Gallon: 18,
-    Cylinders: 8,
-    Displacement: 318,
-    Horsepower: 150,
-    Weight_in_lbs: 3436,
-    Acceleration: 11,
-    Year: "1970-01-01",
-    Origin: "USA",
-  },
-  {
-    Name: "amc rebel sst",
-    Miles_per_Gallon: 16,
-    Cylinders: 8,
-    Displacement: 304,
-    Horsepower: 150,
-    Weight_in_lbs: 3433,
-    Acceleration: 12,
-    Year: "1970-01-01",
-    Origin: "USA",
-  },
-  {
-    Name: "ford torino",
-    Miles_per_Gallon: 17,
-    Cylinders: 8,
-    Displacement: 302,
-    Horsepower: 140,
-    Weight_in_lbs: 3449,
-    Acceleration: 10.5,
-    Year: "1970-01-01",
-    Origin: "USA",
-  },
-];
+const count = 7;
+
 // home url, change it on deployment ...
 const homeUrl = "http://localhost:3000/";
 //grapping HTML elements
@@ -77,7 +22,7 @@ function updateCarListOptions(data) {
 }
 //here we are fetching data from the server and put it in array
 input.addEventListener("keyup", (e) => {
-  const url = `${homeUrl}${autoCompleteApi}?name=${e.target.value}`;
+  const url = `${homeUrl}${autoCompleteApi}?name=${e.target.value}&count=${count}`;
   fetch(url)
     .then((response) => {
       console.log(response);
