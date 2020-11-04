633b222fd1039dc6aee9cfd197317b53a48d0978
Muhammad Awwad <hammode.awad@gmail.com>
2020-11-04 21:52:43 +0200

    css addd, index js modified to support search button(validation)

--- public/index.js
+++ public/index.js
@@ -1,15 +1,16 @@
 const autoCompleteApi = "getcar/";
 let grappedData;
-let wantedSize=7;
+let wantedSize = 7;
+let grappedDataIsUpdated = true;
 // home url, change it on deployment ...
 const homeUrl = "http://localhost:3000/";
 
 
 //grapping HTML elements
+const form = document.querySelector("form");
 const carList = document.querySelector("#cars");
 const input = document.querySelector("#search-input");
-const searchButton=document.querySelector("#search-btn");
-const carContainer=document.querySelector("#car-container");
+const carContainer = document.querySelector("#car-container");
 
 
 //updateCarListOptions() function
@@ -26,27 +27,55 @@ function updateCarListOptions(data) {
     });
 }
 
+//Event listener for the form to show the data in the DOM
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  //here we are checking if the data was fetched(ended).
+  if (grappedDataIsUpdated) {
+    if (input.value !== "") {
+      carContainer.innerHTML="";
+      let carDiv = document.createElement("div");
+      carDiv.id = "car-div";
+      console.log(grappedData);
+      const carObj = grappedData[0];
+      carDiv.innerHTML = `<h3>Car Model: <span>${carObj.Name}</span></h3>
+      <h3>Horse Power: <span>${carObj.Horsepower}</span></h3>
+      <h3>Weight(lbs): <span>${carObj.Weight_in_lbs}</span></h3>
+      <h3>Acceleration: <span>${carObj.Acceleration}</span></h3>
+      <h3>Year: <span>${carObj.Year}</span></h3>
+      <h3>Origin: <span>${carObj.Origin}</span></h3>`;
+      carContainer.appendChild(carDiv);
+      input.value="";
+    }
+    else {
+      alert("Type a car name please!");
+    }
+  }
 
-//here we are fetching data from thes server
+
+});
+
+
+//here we are fetching data (sending a request to the server)
 input.addEventListener("keyup", (e) => {
   const url = `${homeUrl}${autoCompleteApi}?name=${e.target.value}&count=${wantedSize}`;
-  fetch(url)
-    .then((response) => {
-      console.log(response);
-      if (!response.ok) throw new Error("error: failed to receice data ...");
-      return response.json();
-    })
-    .then((data) => {
-      console.log("DATTTTA:",data);
-      if (data.length !== 0) {
-        updateCarListOptions(data);
-        grappedData = data;
-        searchButton.addEventListener("click",()=>{
-          //let carDiv=document.createElement("div");
-        });
-      }
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+  grappedDataIsUpdated = false;
+    fetch(url)
+      .then((response) => {
+        console.log(response);
+        if (!response.ok) throw new Error("error: failed to receice data ...");
+        return response.json();
+      })
+      .then((data) => {
+        if (data.length !== 0) {
+          updateCarListOptions(data);
+          grappedData = data;
+        }
+        grappedDataIsUpdated = true;
+      })
+      .catch((error) => {
+        console.log(error);
+        grappedDataIsUpdated = true;
+      });
+
 });
